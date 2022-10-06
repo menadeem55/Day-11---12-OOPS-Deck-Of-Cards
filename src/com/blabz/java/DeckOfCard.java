77cbc7341f67b7c1145446bfedd0508d9d631bd6
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-06 23:51:15 +0530

    [Added: Deck of Cards games]

---
+++ src/com/blabz/java/DeckOfCard.java
@@ -0,0 +1,43 @@
+package com.blabz.java;
+
+import java.util.Random;
+
+public class DeckOfCard {
+
+	String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+	String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace " };
+	String[][] cards = new String[4][13];
+	String[][] player = new String[4][9];
+
+	public void getCardCombination() {
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 13; j++) {
+				cards[i][j] = ranks[j] + suits[i];
+
+			}
+		}
+	}
+
+	public void getPlayer() {
+
+		// this method is shuffle the cards distribute among 4 player with 9 cards each
+		// randomly
+
+		int i;
+		int j;
+		for (i = 0; i < 4; i++) {
+			System.out.println("\n Cards for Player " + (i + 1) + "\n");
+			for (j = 0; j < 9; j++) {
+				Random random = new Random();
+				int randomSuit = random.nextInt(4);
+				int randomRank = random.nextInt(13);
+				player[i][j] = cards[randomSuit][randomRank];
+				System.out.println(player[i][j] + "  \n");
+
+			}
+		}
+
+	}
+
+}
---
+++ src/com/blabz/java/DeckOfCardMain.java
@@ -0,0 +1,24 @@
+package com.blabz.java;
+
+import java.util.Scanner;
+
+public class DeckOfCardMain {
+	public static void main(String[] args) {
+
+		// calling Deck of card to main methods
+
+		System.out.println("\nPress 1 for view 4 Players cards");
+		DeckOfCard play = new DeckOfCard();
+		Scanner sc = new Scanner(System.in);
+		int press = sc.nextInt();
+
+		switch (press) {
+		case 1:
+			play.getCardCombination();
+			play.getPlayer();
+			break;
+		}
+		sc.close();
+	}
+
+}
